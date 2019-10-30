1b3ffdfbac9db6d977fecc1b31f5f76a847e34c7
Nicola Tournay <nicolatournay@outlook.com>
2019-10-30 16:46:57 +0100

    Yelp mvp: challenge finally completed2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   def new
-    # we need @restaurant in our `simple_form_for`
+    # we need @restaurant iin our `simple_form_for`
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
